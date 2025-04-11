3bea52d24b8e5c46885678ab96c105331dbe8ad7
surflab <surflab@surflab1>
2025-04-11 01:59:36 -0400

    Added dispersion relation plotting. I think it works, but our data is very close to the Nyquist frequency in space and time so it is hard to tell.

--- M-Files_ExpPilot/Eta_vs_t.m
+++ M-Files_ExpPilot/Eta_vs_t.m
@@ -28,6 +28,7 @@ dydt = dy/dt_pair;
 
 DeltaT = nF/2*spp;
 
+mpp = 41.782d-6/0.67; % meters per pixel for corrected Surf Water images.
 
 surfSigmas = [50,40,30,20,10];
 surfSteps = [50,40,30,20];
@@ -80,8 +81,103 @@ for i = 1:nF
     plot(fXs(i,:), -fYs(i,:))
     set(gca,'DataAspectRatio',[1 1 1])
     ylim([-2200,-1900])
-    pause(0.5)
+    pause(0.1)
 end
+%%
+eta = (fYs-mean(fYs(1,1:20)))*mpp;
+eta_var = sum((eta-mean(eta,2)).^2,2)/(size(eta,2)-1);
+
+eta_x = diff(eta,1,2);
+
+eta_x_var = sum((eta_x-mean(eta_x,2)).^2,2)/(size(eta_x,2)-1);
+
+figure(3)
+plot(t,eta_var)
+hold on
+xlabel('Time (s)')
+ylabel('Spacial variance of surface height at each time (m^2)')
+set(gca,'FontSize',24)
+
+figure(4)
+plot(t,eta_x_var)
+hold on
+xlabel('Time (s)')
+ylabel('Spacial variance of surface slope at each time (m/m)^2')
+set(gca,'FontSize',24)
+
+
+figure(5)
+plot(frames,eta_x_var)
+hold on
+xlabel('Frame Number')
+ylabel('Spacial variance of surface slope at each time (m/m)^2')
+set(gca,'FontSize',24)
+%% See https://pordlabs.ucsd.edu/sgille/sioc221a_f19/lecture13_notes.pdf for info on dispersion relation plot from 2D fft
+x = 0:mpp:(size(fYs,2)-1)*mpp;
+
+iti = 1;
+itf = 1800;
+
+figure(6)
+imagesc(x,t(iti:itf),eta(iti:itf,:))
+
+[m,n] = size(eta(iti:itf,:));
+
+f = linspace(0,1/(spp),length(t(iti:itf)));
+
+tfft = nufft(eta(iti:itf,:),t(iti:itf),f,1);
+
+figure(7)
+plot(f,tfft(:,500).*conj(tfft(:,500)))
+ 
+xtfft = fft(tfft,size(tfft,2)*20,2);
+
+k = 2*pi*([1:n*20]-1)/20/n/mpp;
+
+% st = fftshift(xtfft);
+spec = xtfft.*conj(xtfft);
+
+figure(8)
+imagesc(k,f,log10(spec),[0,5.6])
+hold on
+xlim([0,500])
+
+%% See https://pordlabs.ucsd.edu/sgille/sioc221a_f19/lecture13_notes.pdf for info on dispersion relation plot from 2D fft
+x = 0:mpp:(size(fYs,2)-1)*mpp;
+
+iti = 3400;
+itf = 3600;
+
+[X,T] = meshgrid(x,t);
+
+
+test = real(exp(1i*(160*X-2*pi*1*T)));
+
+figure(6)
+imagesc(x,t(iti:itf),test(iti:itf,:))
+
+
+[m,n] = size(test(iti:itf,:));
+
+f = linspace(0,1/(spp),length(t(iti:itf)));
+
+tfft = nufft(test(iti:itf,:),t(iti:itf),f,1);
+
+figure(7)
+plot(f,real(tfft(:,500)))
+
+figure(9)
+imagesc(x,f,real(tfft))
+ 
+xtfft = fft(tfft,size(tfft,2)*20,2);
+
+k = 2*pi*([1:n*20]/20-1)/n/mpp;
+
+% st = fftshift(xtfft);
+spec = xtfft.*conj(xtfft);
+
+figure(8)
+imagesc(k,f,log10(spec))
 
 %%
 function [BadFramePIVSurfW,XPIVSurfW_Surface,PIVSurfW_Surface] = FindWaterSurface(PIVSurfW_CamAngle,surfSigmas, surfSteps, surfMask)
