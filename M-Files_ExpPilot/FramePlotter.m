12a444bab48091a615cc74c3a758b56b5843b8db
surflab <surflab@surflab1>
2025-04-30 11:13:45 -0400

    Plots frames and allows you to enter a frame number or flip back and forth through time.

--- M-Files_ExpPilot/FramePlotter.m
+++ M-Files_ExpPilot/FramePlotter.m
@@ -1,12 +1,15 @@
-DataPath = '/media/surflab/New Volume/ExpPilot/ExpPilot5/ExpPilot5_Scene2/'; %[ROOTPath 'ExpPilot' expName '/' 'ExpPilot' expName '_Scene' sceneName '/' ];
+DataPath = '/media/surflab/New Volume/ExpAW5_acc0.22_W5V/ExpAW5_acc0.22_W5V_Run2/'; %[ROOTPath 'ExpPilot' expName '/' 'ExpPilot' expName '_Scene' sceneName '/' ];
 LoadPath = [DataPath 'RAW/'];
-RawDataPath = [DataPath 'RAW/'];
-ResultsPath = [DataPath 'RESULTS_Andy/'];
 
 PIVWaterDir = dir([LoadPath 'PIVSURF Water/' '*.raw']); %Same for water
 
 
 PIV1Dir_temp = PIVWaterDir;
+
+% -ExpAW1Run2 around 1340 but not as prominant
+% -ExpAW5 Run2 super steep parasitic? capillaries. Perhaps periodic crapper
+% capillaries. solitons? starting around 824. Steep soliton around 861.
+% Some good asymmetry
 
 %%
 viewing = true;
@@ -18,10 +21,12 @@ while viewing
     imagename = [PIV1Dir_temp(idx+1).folder '/' PIV1Dir_temp(idx+1).name];
     [IM1] = load_Image_IOCoreView_12MP(imagename);
     figure(50)
-    imagesc(IM1,[0,85])
+    imagesc(IM1,[0,70])
+    colormap gray
     set(gca,'DataAspectRatio',[1 1 1])
     ip = input("Next Frame?","s")
     ip
+
     if ip == 'a'
         idx = max(idx-1,0);
     elseif ip == 'd'
