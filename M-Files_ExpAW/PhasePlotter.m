1ac59acf31aa1ad226048da2c230720713549fae
surflab <surflab@surflab1>
2025-06-30 08:31:09 -0400

    PhasePlottter.m now fits an exponential curve to the initial wavelet growth phase energy for comparison to Kawai 1979. For loop for edge detection is also modified to include parfor.

--- M-Files_ExpAW/PhasePlotter.m
+++ M-Files_ExpAW/PhasePlotter.m
@@ -4,17 +4,19 @@ clc
 close all
 
 % Define Path
-DataPath = '/media/surflab/New Volume/ExpAW5_acc0.22_W5V/ExpAW5_acc0.22_W5V_Run2/'; %[ROOTPath 'ExpPilot' expName '/' 'ExpPilot' expName '_Scene' sceneName '/' ];
+DataPath = '/media/surflab/Working24/ExpAW/ExpAW5_acc0.22_W5V/ExpAW5_acc0.22_W5V_Run2/'; %[ROOTPath 'ExpPilot' expName '/' 'ExpPilot' expName '_Scene' sceneName '/' ];
 LoadPath = [DataPath 'RAW/'];
 RawDataPath = [DataPath 'RAW/'];
 ResultsPath = [DataPath 'RESULTS_Andy/'];
 
 PIVWaterDir = dir([LoadPath 'PIVSURF Water/' '*.raw']); %Same for water
 %%
-frames = 850:1:870;
+frames = 0:1:2000;
 nF = length(frames);
 fXs = zeros(nF, 3639);
 fYs = zeros(nF, 3639);
+
+mpp = 6.493178e-5; %for main dataset only
 
 pps = 14.5; %pairs per second
 
@@ -36,8 +38,9 @@ DeltaT = nF/2*spp;
 %%
 CompImg = NaN(ceil(DeltaT*dydt),4176);
 framesCtr = 1;
-for idx = frames
-    idx
+tic
+parfor framesCtr = 1:length(frames)
+    idx = frames(framesCtr);
     PIV1Dir_temp = PIVWaterDir;
 
     imagename = [PIV1Dir_temp(idx+1).folder '/' PIV1Dir_temp(idx+1).name];
@@ -53,11 +56,7 @@ for idx = frames
     PIV1_W = [];
     [BadFramePIVSurfW1, XPIVSurfW1_Surface, PIVSurfW1_Surface, XPIVW_PIVSurfW1_Surface, PIVW_PIVSurfW1_Surface, PIVW1_Surface] = ExtractSurface_PIVSurfWater(PIVSurfW1_CamAngle,PIV1_W,'1');
     
-    if mod(idx,2) == 0
-        CompImg(round((framesCtr-1)/2*spp*dydt)+1:round((framesCtr-1)/2*spp*dydt)+dy+1,1:size(PIVSurfW1_CamAngle,2)) = PIVSurfW1_CamAngle(yLimits(1):yLimits(2),:)/mean(PIVSurfW1_CamAngle,'all');
-    else
-        CompImg(round(((framesCtr-2)/2*spp+dt_pair)*dydt)+1:round(((framesCtr-2)/2*spp+dt_pair)*dydt)+dy+1,1:size(PIVSurfW1_CamAngle,2)) = PIVSurfW1_CamAngle(yLimits(1):yLimits(2),:)/mean(PIVSurfW1_CamAngle,'all');
-    end
+
 
     imagesc(PIVSurfW1_CamAngle, [0,70])
     hold on
@@ -68,31 +67,149 @@ for idx = frames
     
     fXs(framesCtr,:) = XPIVSurfW1_Surface;
     fYs(framesCtr,:) = PIVSurfW1_Surface;
-
-    framesCtr = framesCtr + 1;
 end
-%%
+toc
+
+tic
+for framesCtr = 1:length(frames)
+    idx = frames(framesCtr);
+
+    PIV1Dir_temp = PIVWaterDir;
+
+    imagename = [PIV1Dir_temp(idx+1).folder '/' PIV1Dir_temp(idx+1).name];
+    imagename
+    
+    [IM1] = load_Image_IOCoreView_12MP(imagename);
+    PIVSurf_W1_Raw = IM1;
+    PIVSURF_W1 = PIVSurf_W1_Raw./(smooth(mean(PIVSurf_W1_Raw(2000:end,:)),1000)/max(smooth(mean(PIVSurf_W1_Raw(2000:end,:)),1000)))';
+
+    [PIVSurfW1_Undistorted] = PIVSurfW_LensDistCorr(PIVSURF_W1);
+    [PIVSurfW1_CamAngle] = PIVSurfWater_CamAngle_Correction(PIVSurfW1_Undistorted);
+
+    if mod(idx,2) == 0
+        CompImg(round((framesCtr-1)/2*spp*dydt)+1:round((framesCtr-1)/2*spp*dydt)+dy+1,1:size(PIVSurfW1_CamAngle,2)) = PIVSurfW1_CamAngle(yLimits(1):yLimits(2),:)/mean(PIVSurfW1_CamAngle,'all');
+    else
+        CompImg(round(((framesCtr-2)/2*spp+dt_pair)*dydt)+1:round(((framesCtr-2)/2*spp+dt_pair)*dydt)+dy+1,1:size(PIVSurfW1_CamAngle,2)) = PIVSurfW1_CamAngle(yLimits(1):yLimits(2),:)/mean(PIVSurfW1_CamAngle,'all');
+    end
+end
+toc
+%% Plot variance of eta, eta_x. Plot surface area
+t = zeros(1,nF);
+t(1) = floor(frames(1)/2)*spp + mod(frames(1),2)*dt_pair;
+for i = 2:nF
+    if mod(frames(i),2)==1
+        t(i) = t(i-1)+dt_pair;
+    else
+        t(i) = t(i-1)+spp-dt_pair;
+    end
+end
+
+eta = (fYs-mean(fYs(1:20,:),'all'))*mpp;
+x_eta = fXs*mpp;
+eta_var = sum((eta-mean(eta,2)).^2,2)/(size(eta,2)-1);
+
+eta_x = diff(eta/mpp,1,2);
+
+eta_x_var = sum((eta_x-mean(eta_x,2)).^2,2)/(size(eta_x,2)-1);
+
+figure(3)
+ax1 = subplot(3,1,1);
+plot(t,eta_var,'LineWidth',4)
+hold on
+xlabel('Time (s)','Interpreter','latex')
+ylabel('$\mathrm{Var}[\eta]\ \mathrm{(m^2)}$','Interpreter','latex')
+set(gca,'FontSize',24)
+set(gca,'TickLabelInterpreter','latex')
+ylim([0,3e-7])
+
+
+
+ax2 = subplot(3,1,2);
+plot(t,eta_x_var,'LineWidth',4)
+hold on
+xlabel('Time (s)','Interpreter','latex')
+ylabel('$\mathrm{Var}[\eta_x]$','Interpreter','latex')
+set(gca,'FontSize',24)
+set(gca,'TickLabelInterpreter','latex')
+xlim([20,35])
+ylim([0,0.05])
+
+%Calculate Surface Area
+A_surf = zeros(1,length(t));
+for n = 1:length(t)
+    for i = 1:(size(eta,2)-1)
+        A_surf(n) = A_surf(n) + ((eta(n,i+1) - eta(n,i))^2 + (x_eta(n,i+1) - x_eta(n,i))^2).^0.5;
+    end
+end
+
+
+ax3 = subplot(3,1,3);
+l_interface = x_eta(1,end) - x_eta(1,1);
+plot(t,(A_surf - l_interface)/l_interface*100,'LineWidth',4)
+% plot(t,A_surf,'LineWidth',4)
+hold on
+xlabel('Time (s)','Interpreter','latex')
+ylabel('$\mathrm{Surface\ Area\ Increase\ (\%)}$','Interpreter','latex')
+set(gca,'FontSize',24)
+set(gca,'TickLabelInterpreter','latex')
+linkaxes([ax1,ax2,ax3],'x')
+xlim([20,35])
+ylim([0,4])
+
+%% Fit growth rate to initial wavelet growth stage
+ir = [0,0];
+[~,i] = min(abs(t-27));
+ir(1) = i;
+[~,i] = min(abs(t-29.58));
+ir(2) = i;
+ir
+eta_var_fit = fit(t(ir(1):ir(2))',eta_var(ir(1):ir(2)),'exp1')
+% figure
+% plot(eta_var_fit,t(ir(1):ir(2))',eta_var(ir(1):ir(2)))
+
+figure(3)
+hold on
+subplot(3,1,1)
+p1 = plot(t(ir(1):ir(2))',eta_var(ir(1):ir(2)),'.k', 'MarkerSize',15,'DisplayName','Growth Phase Data')
+
+fit_line_t = (t(ir(1)):0.01:t(ir(2)))';
+fit_line = eta_var_fit.a.*exp(fit_line_t.*eta_var_fit.b);
+s = sprintf('Exponential Fit, Growth Rate %.2f 1/s',eta_var_fit.b);
+p2 = plot(fit_line_t, fit_line, '-r', 'LineWidth',3,'DisplayName',s)
+legend([p1,p2],'Interpreter','latex')
+%% Plot hovemollerish thing
 P_threshold = 10;
+
 
 figure(2)
 
-mpp = 6.493178e-5; %for main dataset only
 x = (1:size(CompImg,2))*mpp;
 y = 1:size(CompImg,1);
 t = y/dydt;
 
+hold off
 imagesc(CompImg,'XData',x,'YData',t,[0,3])
 hold on
-xlabel('x (m)')
-ylabel('t (s)')
-s = DataPath(end-23:end-1) + " " + frames(1) + " to " + frames(end);
-title(s,'Interpreter','none')
+
+xlabel('x (m)','Interpreter','latex')
+ylabel('t (s)','Interpreter','latex')
+
+
 set(gca,'DataAspectRatio',[1*mpp 1/dydt 1])
-set(gca, 'ytick', 0:spp:t(end));
+set(gca, 'ytick', 0:spp:t(end),'TickLabelInterpreter','latex');
 set(gca, 'xtick', 0:0.02:x(end));
 set(gca,'FontSize',24)
 colormap gray
-% axis off
+
+ylim([29,30.03]);
+yl = ylim;
+%s = DataPath(end-23:end-1) + " " + frames(1) + " to " + frames(end);
+il = [0,0];
+il(1) = floor(yl(1)/spp)*2 + min(floor(mod(yl(1),spp)/dt_pair),1);
+il(2) = floor(yl(2)/spp)*2 + min(floor(mod(yl(2),spp)/dt_pair),1);
+s = sprintf('%s Frames %d to %d', [DataPath(end-23:end-18), '\_', DataPath(end-16:end-10), '\_', DataPath(end-8:end-6), '\_', DataPath(end-4:end-1)], frames(il(1)), frames(il(2)));
+title(s,'Interpreter','latex')
+
 
 minPts = NaN(300,2);
 cmp = 1;
