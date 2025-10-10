171fb60e00103fd0aaf797523e4a71a4b570d71d
Andrew Goering <andrew.goering1@gmail.com>
2025-10-10 10:04:01 -0400

    Added test script I with modifications I made in Delaware to allow for flipping through frames and plotting PIV results. Based on Main_LC_2023.m.

---
+++ M-Files_FabMarcNovDec2014/Main_LC_2023_Plotting.m
@@ -0,0 +1,178 @@
+LONG=['/media/surflab/LC_Working24/LC/FabMarcNovDec2014/data/Longitudinal/PIVdt10ms_IRlas1_8hz/']
+DIRS=dir(LONG);
+DIRS=DIRS(3:end);
+
+for ii=1:length(DIRS)
+
+exp_name=DIRS(ii).name;
+
+num_of_digits = 3;
+load_path = [LONG exp_name];
+files=dir([load_path '/PIVRaw/PIV/*.mat']);
+number_of_pair=length(files)/2;
+
+%%%%%%%% Stuff added by Andy to quickly flip through frames. should be
+%%%%%%%% commented out for normal use
+
+image_pair_number = 123;
+while image_pair_number < number_of_pair-1
+    load([load_path '/PIVRaw/PIV/' exp_name '_Piv_' sprintf(['%0' num2str(num_of_digits) 'd'], image_pair_number) '_a.mat']); %replace ~ with path
+    IM_a = imgPiv;
+    load([load_path '/PIVRaw/PIV/' exp_name '_Piv_' sprintf(['%0' num2str(num_of_digits) 'd'], image_pair_number) '_b.mat']); %replace ~ with path
+    IM_b = imgPiv;
+
+    %PIV Surf
+    load([load_path '/PIVRaw/PIVSURF/' exp_name '_Pivsurf_' sprintf(['%0' num2str(num_of_digits) 'd'], image_pair_number) '_a.mat']); %replace ~ with path
+    imgPivsurf1 = imgPivsurf;
+    load([load_path '/PIVRaw/PIVSURF/' exp_name '_Pivsurf_' sprintf(['%0' num2str(num_of_digits) 'd'], image_pair_number) '_b.mat']); %replace ~ with path
+    imgPivsurf2 = imgPivsurf;
+    
+    disp(['image_pair_number = ', num2str(image_pair_number)])
+    figure(1)
+    imagesc(imgPivsurf1, [0,300])
+    figure(2)
+    imagesc(imgPivsurf2, [0,300])
+    
+    ip = input('a for back, d for forward','s');
+    nip = str2double(ip);
+    if ip == 'a'
+        image_pair_number = max(0,image_pair_number-1);
+    elseif ip == 'd'
+        image_pair_number = min(number_of_pair-1, image_pair_number+1);
+    elseif ~isnan(nip) && floor(nip) == nip && image_pair_number >= 0 && image_pair_number < number_of_pair
+        image_pair_number = nip;
+    end
+
+end
+
+%%%%%% End of stuff added by Andy to quickly flip through frames.
+
+for image_pair_number=0:number_of_pair-1
+%PIV
+load([load_path '/PIVRaw/PIV/' exp_name '_Piv_' sprintf(['%0' num2str(num_of_digits) 'd'], image_pair_number) '_a.mat']); %replace ~ with path
+IM_a = imgPiv;
+load([load_path '/PIVRaw/PIV/' exp_name '_Piv_' sprintf(['%0' num2str(num_of_digits) 'd'], image_pair_number) '_b.mat']); %replace ~ with path
+IM_b = imgPiv;
+ 
+%PIV Surf
+load([load_path '/PIVRaw/PIVSURF/' exp_name '_Pivsurf_' sprintf(['%0' num2str(num_of_digits) 'd'], image_pair_number) '_a.mat']); %replace ~ with path
+imgPivsurf1 = imgPivsurf;
+load([load_path '/PIVRaw/PIVSURF/' exp_name '_Pivsurf_' sprintf(['%0' num2str(num_of_digits) 'd'], image_pair_number) '_b.mat']); %replace ~ with path
+imgPivsurf2 = imgPivsurf;
+
+%Surface detection and Creating Masks
+U1 = [147 49;2024 57; 1995 1004; 161 999];
+X1 = [147 49; 2024 49; 2024 1004; 147 1004];
+T1 = fitgeotrans(U1,X1,'projective');
+d1=imwarp(imgPivsurf1,T1,'cubic');
+d2=imwarp(imgPivsurf2,T1,'cubic');
+
+d1=imresize(d1,176.9769/105.5880);%Resizing to match PIV
+d2=imresize(d2,176.9769/105.5880);%Resizing to match PIV
+s1=d1(30:3525,755:755+2047); %croping
+s2=d2(30:3525,755:755+2047);
+
+%%%%%OG Fabrice surface detection
+% imSurf1 = findSurface_simple_ext_force_2023((medfilt2(s1)), 1);
+% imSurf2 = findSurface_simple_ext_force_2023((medfilt2(s2)), 1);
+
+%%%%%Andy Crapper-optimized surface detection
+surfSigmas = [50 40 30 20 10 8 6];
+surfSteps = [50 40 30 20 10 8];
+SurfMask = 1;
+imSurf1 = CrapperOptimized_FindSurface(s1, surfSigmas, surfSteps, 1);
+imSurf2 = CrapperOptimized_FindSurface(s2, surfSigmas, surfSteps, 1);
+
+imSurf1.surface=imSurf1.surface-1716+287;
+imSurf2.surface=imSurf2.surface-1716+287;
+[h, w] = size(IM_a); %image height and width
+mask1=ones(size(IM_a));
+mask2=ones(size(IM_a));
+for i=1:w
+    mask1(1:round(imSurf1.surface(i)),i)=NaN;
+    mask2(1:round(imSurf2.surface(i)),i)=NaN;
+end
+warning off
+imSurf1.mask=mask1;
+imSurf2.mask=mask2;
+
+IntrWndw=[256 128 64 32 16 8];
+GrdSpc=[128 64 32 16 8 4];
+compVel =  computeVelocities_marc_quick_nofilt(IM_a, IM_b, mask1, mask2, IntrWndw, GrdSpc);
+compVel.DX=1/17697.69; %m per pix
+compVel.DT=10d-3; % sec per image pair -  DELTA_T= 10 milisec
+
+%% Plot PIV
+figure(3)
+hold off
+imagesc((1:w)*compVel.DX, (1:h)*compVel.DX, compVel.delx.*compVel.mask*compVel.DX/compVel.DT)
+hold on
+colormap gray
+set(gca, 'TickLabelInterpreter','latex', 'FontSize', 20);
+c = colorbar;
+c.TickLabelInterpreter = 'Latex';
+c.Label.String = 'u (m/s)';
+c.Label.Interpreter = 'latex';
+xlabel('x (m)', 'Interpreter','latex')
+ylabel('z (m)', 'Interpreter', 'latex')
+titleStr = sprintf('ExpLC%s-%s Pair Number %d', exp_name(end-3:end-3), exp_name(end-1:end), image_pair_number);
+title(titleStr,'Interpreter','latex')
+daspect([1,1,1])
+
+figure(4)
+hold off
+imagesc((1:w)*compVel.DX, (1:h)*compVel.DX, compVel.dely.*compVel.mask*compVel.DX/compVel.DT)
+hold on
+colormap gray
+set(gca, 'TickLabelInterpreter','latex', 'FontSize', 20);
+c = colorbar;
+c.TickLabelInterpreter = 'Latex';
+c.Label.String = 'w (m/s)';
+c.Label.Interpreter = 'latex';
+xlabel('x (m)', 'Interpreter','latex')
+ylabel('z (m)', 'Interpreter', 'latex')
+titleStr = sprintf('ExpLC%s-%s Pair Number %d', exp_name(end-3:end-3), exp_name(end-1:end), image_pair_number);
+title(titleStr,'Interpreter','latex')
+daspect([1,1,1])
+
+%% Save results
+
+% outfile = [load_path '/PIVMat_2023/' exp_name '_compVel_' sprintf(['%0' num2str(num_of_digits) 'd'], image_pair_number)];
+% save(outfile, 'compVel', 'imSurf1', 'imSurf2');
+% disp(['pair ' num2str(image_pair_number) ' velocity done.']);
+
+%%
+imSurf = findSurface_simple_ext_force_2023((medfilt2(s1)), 1);
+Surface_PIV=imSurf.surface;
+
+pivRes.zPIV = compVel.zPIV;
+pivRes.xPIV = compVel.xPIV;
+pivRes.GS = compVel.GS;
+pivRes.mask = compVel.mask;
+
+transfo = generateTransfo_LC_noLFV_2023( compVel, Surface_PIV, pivRes); % 0 is there to compare SU(0,:) with surface
+
+SU = transfo.SU; 
+SU = SU(2:end,:); % all but surface; % first line is zeta=0 THE SURFACE EXACTELY
+SU = SU -1716+287;
+ORBX = transfo.ORBX;
+ORBX = ORBX(2:end,:);
+ORBZ = transfo.ORBZ;
+ORBZ = ORBZ(2:end,:);
+%
+pivRes.GS = compVel.GS;
+pivRes.zPIV = compVel.zPIV;
+pivRes.pf_surf =SU(1,:);
+
+u = compVel.delx.*compVel.mask;
+w = compVel.dely.*compVel.mask;
+%
+intrp_u = transformVelField_decay_forFab( u, pivRes, SU );
+intrp_w = transformVelField_decay_forFab( w, pivRes, SU );
+
+intU_minusORBX = intrp_u - ORBX;
+intW_minusORBZ = intrp_w - ORBZ;
+
+u_turb = reverseTransformVelField_decay_forFab( intU_minusORBX, pivRes, SU );
+w_turb = reverseTransformVelField_decay_forFab( intW_minusORBZ, pivRes, SU );
+uuTest = reverseTransformVelField_decay_forFab( intrp_u, pivRes, SU);
