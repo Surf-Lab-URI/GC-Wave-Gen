439b12c6532a4a23e33a7a388a4cf30b714703d5
Andrew Goering <andrew.goering1@gmail.com>
2026-02-25 20:09:34 -0500

    Made offset optional. Now you can just perform the cropping which is what you would want if you were applying the function to an eta variable that has already been demeaned

--- M-Files_FabMarcNovDec2014/CropSurfToPIVDims.m
+++ M-Files_FabMarcNovDec2014/CropSurfToPIVDims.m
@@ -1,7 +1,10 @@
-function surfCropped = CropSurfToPIVDims(surf)
+function surfCropped = CropSurfToPIVDims(surf,verticalCorrection)
 %CROPSURFTOPIVDIMS Cropps surface elevation array from size of the surface
 %image with small crop to remove lense distortion that has been rescaled to
 %PIV coordinates to the size of the PIV image
-surfCropped = surf(655:655+2047)-1716+287;
+surfCropped = surf(655:655+2047);
+if verticalCorrection
+    surfCropped = surfCropped-1716+287;
+end
 end
 
